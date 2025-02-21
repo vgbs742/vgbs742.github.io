d29479db54b3e81c50b95371bead5b8248c919e7
Johann Keller <jkjoehu88@gmail.com>
2025-02-21 12:49:15 +0100

    +datenschutzerklärung

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -242,5 +242,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../src/content.config.js");
+	export type ContentConfig = typeof import("../src/content.config.js");
 }
