cf7f1eeee6649048a31e71ddf8d1d2ef0dd25621
Johann Keller <jkjoehu88@gmail.com>
2025-04-02 22:53:56 +0200

    otherblue

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,16 +1,16 @@
 
 export default new Map([
-["src/collections/sections/fr/news.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Ffr%2Fnews.mdx&astroContentModuleFlag=true")],
-["src/collections/sections/de/general.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fgeneral.mdx&astroContentModuleFlag=true")],
-["src/collections/sections/de/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fnow.mdx&astroContentModuleFlag=true")],
-["src/collections/now/de/3-Ueberfuehrung.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnow%2Fde%2F3-Ueberfuehrung.mdx&astroContentModuleFlag=true")],
 ["src/collections/sections/en/news.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fen%2Fnews.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/de/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2FintroText01.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/de/verein.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2Fverein.mdx&astroContentModuleFlag=true")],
+["src/collections/sections/fr/news.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Ffr%2Fnews.mdx&astroContentModuleFlag=true")],
+["src/collections/sections/de/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fnow.mdx&astroContentModuleFlag=true")],
+["src/collections/sections/de/general.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fsections%2Fde%2Fgeneral.mdx&astroContentModuleFlag=true")],
+["src/collections/now/de/3-Ueberfuehrung.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnow%2Fde%2F3-Ueberfuehrung.mdx&astroContentModuleFlag=true")],
 ["src/collections/now/de/2-asbest.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnow%2Fde%2F2-asbest.mdx&astroContentModuleFlag=true")],
 ["src/collections/now/de/1-stellplatz.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fnow%2Fde%2F1-stellplatz.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/en/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fen%2FintroText01.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/de/spenden.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2Fspenden.mdx&astroContentModuleFlag=true")],
 ["src/collections/general/de/DSE.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2Fgeneral%2Fde%2FDSE.mdx&astroContentModuleFlag=true")],
-["src/collections/bigBits/fr/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Ffr%2FintroText01.mdx&astroContentModuleFlag=true")]]);
+["src/collections/bigBits/de/spenden.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2Fspenden.mdx&astroContentModuleFlag=true")],
+["src/collections/bigBits/de/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2FintroText01.mdx&astroContentModuleFlag=true")],
+["src/collections/bigBits/fr/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Ffr%2FintroText01.mdx&astroContentModuleFlag=true")],
+["src/collections/bigBits/en/introText01.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fen%2FintroText01.mdx&astroContentModuleFlag=true")],
+["src/collections/bigBits/de/verein.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcollections%2FbigBits%2Fde%2Fverein.mdx&astroContentModuleFlag=true")]]);
 		
